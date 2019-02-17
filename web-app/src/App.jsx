0e85fbc443fc84cc07a3221d4b56393c04fecb3d
Viacheslav Makarov <makarov.viacheslav@gmail.com>
2019-02-17 16:53:56 +0300

    Try pipeline operator with RxJS

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react"
+import { Observable, of } from "rxjs"
+import { map, tap } from "rxjs/operators"
 
 export function App(props) {
+
+    let result = 0
+    const value = of(2019)
+        |> map(x => x * 2)
+        |> map(x => x + 1)
+        |> (v => v.subscribe(x => { result = x }))
+
     return (
-        <p>Hello World</p>
+        <p>Hello World: {result}</p>
     )
 }
